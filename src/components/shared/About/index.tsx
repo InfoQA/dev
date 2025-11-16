3a3d5cd63908caa34b2695ea6b04c18bca2df690
Elmosius Suli <elmosius18@gmail.com>
2025-11-16 20:37:24 +0700

    feat: make modal informasi about

--- src/components/shared/About/index.tsx
+++ src/components/shared/About/index.tsx
@@ -1,6 +1,14 @@
 import INFO_APP from "@/components/constants";
 import {Button} from "@/components/ui/button.tsx";
 import useIsMobile from "@/hooks/useIsMobile.ts";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger
+} from "@/components/ui/dialog.tsx";
 
 export default function About() {
     const isMobile = useIsMobile()
@@ -14,9 +22,30 @@ export default function About() {
                         {INFO_APP.NAME} {INFO_APP.VERSION}
                     </p>
 
-                    <Button variant={'outline'} size={'icon-sm'} className={'text-sm rounded-full'}>
-                        ?
-                    </Button>
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <Button variant={'outline'} size={'icon-sm'} className={'text-sm rounded-full'}>
+                                ?
+                            </Button>
+                        </DialogTrigger>
+                        <DialogContent className={'p-8 lg:max-w-xl'}>
+                            <DialogHeader>
+                                <DialogTitle className={'text-sm font-semibold'}>
+                                    {INFO_APP.NAME}
+                                </DialogTitle>
+                                <DialogDescription className={'text-xs mt-2'}>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                    deserunt mollit anim id est laborum.
+                                </DialogDescription>
+                            </DialogHeader>
+
+                        </DialogContent>
+                    </Dialog>
+
                 </div>
             </div>
         )
